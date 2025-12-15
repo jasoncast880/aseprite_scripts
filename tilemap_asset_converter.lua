03a319ae0af88ee84123f07d17fb8bf8b4c2f1a6
plebish <jasoncast880@gmail.com>
2025-12-14 19:57:25 -0500

    tileset converer written; Needs fixes and testing

--- tilemap_asset_converter.lua
+++ tilemap_asset_converter.lua
@@ -1,18 +1,112 @@
--- get the active sprite, cel, img
-local activeSprite = app.sprite
-local activeCel = app.cel
-local layer = activeCel.layer
-local activeImg = app.image
+--all frames on the layer, each frame gets its own handle
+--ASSUME: all frames have same tile dims: ie frame 1 and frame 5 both are 3x5 tiles
 
--- iterate through tile entries in the tilemap image
-local tileIndices = {}
-local pc = app.pixelColor
 
-for pixel in activeImage:pixels() do
-	local tileIndex = pc.tileI(pixel())
-	table.insert(tileIndices, tileIndex)
+--form/fe construct
+local d = Dialog("Convert Tilemap to Asset File") --SEPERATE FILE for tileset layer
+d:number{ id="tile_len", label="Tile Size", text="16", focus=true, }
+	:entry{ id="filter_color",label="Filter Color", text="0x6767", visible=false }
+	:check{ id="check_sprite", label="Sprite Mode", selected=false,
+	onclick=function()
+		d:modify{
+			id="filter_color",
+			visible=d.data.check_sprite
+		}
+	end   }
+	:entry{ id="output_filepath",label="Filepath", text="C://Downloads" }
+	:button{ id="confirm_button", text="CONFIRM" }
+:show()
+
+--globs and assertions
+local data = d.data
+
+local sprite = app.sprite
+local frame = app.frame
+local layer = app.layer
+
+assert((frame.frameNumber == 1 and layer.stackIndex == 1), "GO TO THE FIRST FRAME, FIRST LAYER TO RUN THIS SCRIPT")
+assert(layer.tileset, "NON-VALID FORMAT (not tilemap)")
+
+--button handler
+if(data.confirm_btn) then
+	local tile_len = data.tile_len
+	local filter_color = data.filter_color
+
+	local file, err = io.open("output.h", "a")
+	assert(file, err)
+
+	--assertion step end TODO : assert filepath validity, tileset existence.
+	local img = Image(sprite.spec)
+	local tiles_w = img.width / tile_len
+	local tiles_h = img.height / tile_len
+
+	--file manipulation here
+	file:write("#ifndef SPRITE_CONF_TEST_H \n #define SPRITE_CONF_TEST_H \n") -- can be changed based on the context. this for tactigachi
+	file:write("#include <cstdint> \n")
+
+	local tileset_size = layer.tileset.grid.tileSize
+	local tileset_pixels = {}
+
+	for i=1, tileset_size+1 do
+		local tile_image = layer.tileset:getTile(i)
+		for it in tile_image:pixels() do
+			table.insert(tileset_pixels, it())
+		end
+	end
+
+	AppendHeaderTileset(file, tileset_pixels, filter_color)
+
+	local indices = {}
+	local tilemap_pos = 0 -- 0-based indexing for C++ 
+	while(frame) do
+		indices = ReadFrameIndices(frame)
+		AppendHeaderTilemap(file, indices, tilemap_pos, tiles_w, tiles_h)
+
+		frame = frame.next
+		tilemap_pos = tilemap_pos+1
+	end
+
+	file:write("#endif") -- can be changed based on the context. this for tactigachi
 end
--- local func:
--- print the list of indices and return a table of tileIndeces.
+--EOF
 
--- write a file with table, holding indeces list.
+function ReadFrameIndices(f)
+	local tileIndices = {}
+
+	local pc = app.pixelColor
+	local img = layer:cel(f).image --!
+
+	for pixel in img:pixels() do
+		local tileIndex = pc.tileI(pixel())
+		table.insert(tileIndices, tileIndex)
+	end
+
+	return tileIndices
+end
+
+--assuming global, asserted file handle in 'append mode' named file
+function AppendHeaderTilemap(file, table, p, w, h)
+	file:write("inline uint8_t testSpritePos_" .. p .. "[]={")
+	for i=1, h+1 do
+		for j=1, w+1 do
+			file:write(table[i*w+j] .. ", ")
+		end
+		file:write("\n")
+	end
+	file:write("};")
+end
+
+function AppendHeaderTileset(file, table, filter_color)
+	file:write("inline uint8_t testSpriteTILESET[]={")
+	for i=0, #table do
+		for j=0,16 do
+			if (table[i*16+j] == {ALPHA_COLOR}) then -- TODO: establish how the pixels are stored, and write method to conver them to rgb 565
+				file:write(filter_color .. ",")
+			else
+			file:write(table[i*16+j] .. ",")
+		end
+		file:write("\n")
+	end
+	file:write("};")
+end
+
