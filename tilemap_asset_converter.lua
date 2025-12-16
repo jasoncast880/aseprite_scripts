4c3077e9f813d66000792bdd2f20c3db14fdda5d
plebish <jasoncast880@gmail.com>
2025-12-16 16:41:17 -0500

    Last time i ran the script something terrible happened to my win machine

--- tilemap_asset_converter.lua
+++ tilemap_asset_converter.lua
@@ -1,7 +1,7 @@
 --all frames on the layer, each frame gets its own handle
 --ASSUME: all frames have same tile dims: ie frame 1 and frame 5 both are 3x5 tiles
 
-function ReadFrameIndices(f, pc, img)
+function ReadFrameIndices(pc, img)
 	local tileIndices = {}
 
 	for pixel in img:pixels() do
@@ -12,7 +12,7 @@ function ReadFrameIndices(f, pc, img)
 	return tileIndices
 end
 
-function AppendHeaderTilemap(file, tbl, p, w, h, filename)
+function AppendTilemap(file, tbl, p, w, h, filename)
 	file:write("inline uint8_t " .. filename .. "Pos_" .. p .. "[]={")
 	local idx=1
 	for y=1, h do
@@ -25,7 +25,7 @@ function AppendHeaderTilemap(file, tbl, p, w, h, filename)
 	file:write("};")
 end
 
-function AppendHeaderTileset(file, tbl, filename)
+function AppendTileset(file, tbl, filename)
 	file:write("inline uint8_t " .. filename .. "Tileset[] ={")
 	for i=0, (#tbl/16) do
 		for j=1,(16+1) do
@@ -36,7 +36,8 @@ function AppendHeaderTileset(file, tbl, filename)
 	file:write("};")
 end
 
-function GetFileHandle(filename)
+-- currently will output in some working directory of the aseprite app (tested on windows)
+function GetFileHandle(filename) -- can be rewritten to accomodate diff os formats, download locations
 	local file, err = io.open(filename..".h", "a")
 	assert(file, err)
 	return file
@@ -49,6 +50,7 @@ local layer = app.layer
 --form/fe construct
 local d = Dialog("Convert Tilemap to Asset File") --SEPERATE FILE for tileset layer
 d:number{ id="tile_len", label="Tile Size", text="16", focus=true, }
+	:number{ id="num_frames", label="# of Frames", text="1" }
 	:entry{ id="filter_color",label="Filter Color", text="0x6767", visible=false }
 	:check{ id="check_sprite", label="Sprite Mode", selected=false,
 	onclick=function()
@@ -70,6 +72,7 @@ assert(layer.tileset, "NON-VALID FORMAT (not tilemap)")
 --button handler
 if(data.confirm_button) then
 	local tile_len = data.tile_len
+	local num_frames = data.num_frames
 	local filter_color = data.filter_color
 	local filename = data.filename
 
@@ -81,21 +84,21 @@ if(data.confirm_button) then
 	local tiles_h = sprite_img.height / tile_len
 
 	--file manipulation here
-	file:write("#ifndef " .. data.filename .. "\n#define " .. data.filename .. "\n") -- can be changed based on the context. this for tactigachi
+	file:write("#ifndef " .. data.filename .. "\n#define " .. data.filename .. "\n") -- can be changed based on the context.
 	file:write("#include <cstdint> \n")
 
 	--get the tileset pixels, format as rgb 565
 	local tileset = layer.tileset
 	local tileset_pixels = {}
+	local num_tiles = num_frames * tiles_w * tiles_h
 
 	--tilset pixel extraction loops
-	local i=1
-	while(true) do
-		local tile_image = tileset:getTile(i)
+	for i=1, num_tiles do --HOW TO GET NUM TILES
+		local tile_image = tileset:tile(i).image -- @@@ problems staart here !!!
+		local pc = app.pixelColor
+
 		for it in tile_image:pixels() do
-			local pc = app.pixelColor
-			local pixValue = it() --manipulat this value to become the rgb 565 pixel 
-		
+			local pixValue = it() --manipulate this value to become the rgb 565 pixel 
 			if(pc.rgbaA(pixValue)==(0)) then
 				table.insert(tileset_pixels, filter_color)
 			else
@@ -112,25 +115,24 @@ if(data.confirm_button) then
 				table.insert(tileset_pixels, string.format( "0x%04X", byte_565))
 			end
 		end
-
-		local next_image = tileset:getTile(i+1)
-		if next_image == nil then break end
 	end
 
-	AppendHeaderTileset(file, tileset_pixels, filename)
+	AppendTileset(file, tileset_pixels, filename)
+	print(string.format("Tileset Completed \n TOTAL PIXELS: %d \n TOTAL TILES: %d \n TOTAL DATA: ~%d(by) ", #tileset_pixels, num_tiles, (#tileset_pixels*2))
+	--tileset portion finished
 
 	local indices = {}
-	local tilemap_pos = 0 -- 0-based indexing for C++ 
 	while(frame) do
 		local pc = app.pixelColor
 		local img = layer:cel(frame).image --!
 
-		indices = ReadFrameIndices(frame, pc, img)
-		AppendHeaderTilemap(file, indices, tilemap_pos, tiles_w, tiles_h, filename)
+		indices = ReadFrameIndices(pc, img)
+		AppendTilemap(file, indices, frame.frameNumber, tiles_w, tiles_h, filename)
 
 		frame = frame.next
-		tilemap_pos = tilemap_pos+1
 	end
 
-	file:write("#endif") -- can be changed based on the context. this for tactigachi
+	file:write("\n#endif") -- can be changed based on the context. this for tactigachi
 end
+
+
